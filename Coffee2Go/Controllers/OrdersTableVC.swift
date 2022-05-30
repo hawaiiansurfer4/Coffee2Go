625c442c509e3e1c07f521b8045e449b13567a7f
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-30 19:24:22 -0400

    Connects OrderListViewModel to OrdersTableVC

--- Coffee2Go/Controllers/OrdersTableVC.swift
+++ Coffee2Go/Controllers/OrdersTableVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class OrdersTableVC: UITableViewController {
     
+    var orderListVM = OrderListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populateOrders()
@@ -22,13 +24,33 @@ class OrdersTableVC: UITableViewController {
         
         let resourse = Resource<[Order]>(url: coffeeOrdersURL)
         
-        WebService().load(resource: resourse) { result in
+        WebService().load(resource: resourse) { [weak self] result in
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.orderListVM.ordersViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.orderListVM.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vm = self.orderListVM.orderViewModel(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableVieCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        
+        return cell
     }
 }

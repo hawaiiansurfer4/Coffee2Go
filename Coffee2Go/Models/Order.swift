22484a248e3a1405922f047a0d538c87d3fbe641
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-30 17:16:16 -0400

    Adds Order model with CoffeeType and CoffeeSize enums

---
+++ Coffee2Go/Models/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  Coffee2Go
+//
+//  Created by Sean Murphy on 5/30/22.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}

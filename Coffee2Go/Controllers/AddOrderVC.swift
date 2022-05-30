772821f4b3110f372c28e34a775006b63b576401
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-30 14:59:42 -0400

    Adds OrdersTableVC and AddOrderVC with respective files

---
+++ Coffee2Go/Controllers/AddOrderVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffee2Go
+//
+//  Created by Sean Murphy on 5/30/22.
+//
+
+import UIKit
+
+class AddOrderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coffee2Go/Controllers/OrdersTableVC.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableVC.swift
+//  Coffee2Go
+//
+//  Created by Sean Murphy on 5/30/22.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableVC: UITableViewController {
+    
+}

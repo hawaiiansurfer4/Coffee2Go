cdddb6e20537c418b0d7a6963a4c153b84c44e4f
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-30 19:07:53 -0400

    Adds OrderViewModel and OrderListViewModel

---
+++ Coffee2Go/VIew Models/OrderViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  OrderViewModel.swift
+//  Coffee2Go
+//
+//  Created by Sean Murphy on 5/30/22.
+//
+
+import Foundation
+
+class OrderListViewModel {
+    
+    var ordersViewModel: [OrderViewModel]
+    
+    init() {
+        self.ordersViewModel = [OrderViewModel]()
+    }
+}
+
+extension OrderListViewModel {
+    
+    func orderViewModel(at index: Int) -> OrderViewModel {
+        return self.ordersViewModel[index]
+    }
+}
+
+struct OrderViewModel {
+    let order: Order
+}
+
+extension OrderViewModel {
+    
+    var name: String {
+        return self.order.name
+    }
+    
+    var email: String {
+        return self.order.email
+    }
+    
+    var type: String {
+        return self.order.type.rawValue.capitalized
+    }
+    
+    var size: String {
+        return self.order.size.rawValue.capitalized
+    }
+}

751683bd477c9c07ac4ed1c7c0b07c3c89f70ef0
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-30 18:48:17 -0400

    Adds WebService call

--- Coffee2Go/Controllers/OrdersTableVC.swift
+++ Coffee2Go/Controllers/OrdersTableVC.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class OrdersTableVC: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateOrders()
+    }
+    
+    private func populateOrders() {
+        guard let coffeeOrdersURL = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else {
+            fatalError("URL was incorrect")
+        }
+        
+        let resourse = Resource<[Order]>(url: coffeeOrdersURL)
+        
+        WebService().load(resource: resourse) { result in
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
--- Coffee2Go/Services/WebService.swift
+++ Coffee2Go/Services/WebService.swift
@@ -20,7 +20,9 @@ struct Resource<T: Codable> {
 class WebService {
     
     func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        
         URLSession.shared.dataTask(with: resource.url) { data, resp, error in
+            
             guard let safeData = data, error == nil else {
                 completion(.failure(.domainError))
                 return
@@ -34,6 +36,6 @@ class WebService {
             } else {
                 completion(.failure(.decodingError))
             }
-        }
+        }.resume()
     }
 }
